c68c086ccde4e9b1be7d236d4efe3a0a55c7ba7e
m6k4 <olszewskazuzanna95@gmail.com>
2021-11-04 21:43:17 +0100

    initial

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getList(Request $request) {
+        return response()->json(User::all());
+    }
+}
---
+++ database/migrations/2021_11_04_202150_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('password');
+            $table->string('email');
+        });
+
+        DB::table('users')->insert(
+            array(
+                'name' => 'zuza',
+                'email' => 'name@domain.com',
+                'password' => 'Qwerty123',
+                'surname' => 'zyuuuz'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
